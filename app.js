18012cdc82a763c7023d3fabaad4f61f88e9385f
bspang <bspangler21@alumni.uwosh.edu>
2024-02-10 01:46:30 -0600

    Install debug and chalk

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
+// Colorize our debug messages
+const chalk = require("chalk");
+// Debug is a small debugging utility module that is used to debug a Node.js application
+const debug = require("debug")("app");
 
 // Create an Express application
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+	res.send("Hello World!");
 });
 
 // Start the server
 app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000');
+	console.log(`listening on port ${chalk.green("3000")}`);
 });
-
