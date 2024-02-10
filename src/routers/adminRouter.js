df35b2d8eebf2eb1567558714a4727b2149c376b
bspang <bspangler21@alumni.uwosh.edu>
2024-02-10 02:33:24 -0600

    Add MONGO_URI to package.json and update adminRouter.js to connect to MongoDB

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -5,12 +5,18 @@ const { MongoClient } = require("mongodb");
 const adminRouter = express.Router();
 
 adminRouter.route("/").get((req, res) => {
-	mongodb.MongoClient;
+	const url = process.env.MONGODB_URI || "";
+	const dbName = "golf-tracker";
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+    }
+    catch(error){
+      debug(error.stack);
+    }
+  })
 });
-
-
-
-
-
 
 module.exports = adminRouter;
