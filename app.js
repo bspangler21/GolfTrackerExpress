5f52088c038aaef24118e025f62e445e3d965893
bspang <bspangler21@alumni.uwosh.edu>
2024-02-10 01:49:15 -0600

    Add debug message for app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const chalk = require("chalk");
 // Debug is a small debugging utility module that is used to debug a Node.js application
 // Only runs in debug mode. Run this by running DEBUG=* node app.js
 // or set DEBUG=* & node app.js for Windows
+// or set debug=app & node app.js to only get debug messages from app
+
 const debug = require("debug")("app");
 
 // Create an Express application
