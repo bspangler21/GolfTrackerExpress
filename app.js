16ec18925f3d7d92241148643a1e5513c04b81bf
bspang <bspangler21@alumni.uwosh.edu>
2024-02-10 01:48:26 -0600

    Implement debug

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 // Colorize our debug messages
 const chalk = require("chalk");
 // Debug is a small debugging utility module that is used to debug a Node.js application
+// Only runs in debug mode. Run this by running DEBUG=* node app.js
+// or set DEBUG=* & node app.js for Windows
 const debug = require("debug")("app");
 
 // Create an Express application
@@ -13,5 +15,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(5000, () => {
-	console.log(`listening on port ${chalk.green("3000")}`);
+	debug(`listening on port ${chalk.green("3000")}`);
 });
