6054dd91c4d72300317e801c70b97cb49d6ce1b4
bspang <bspangler21@alumni.uwosh.edu>
2024-02-11 00:09:16 -0600

    Working routes for getting Golfers

--- src/routers/golfersRouter.js
+++ src/routers/golfersRouter.js
@@ -1,20 +1,69 @@
 const express = require("express");
+const debug = require("debug")("app:golfersRouter");
+const { MongoClient, ObjectID } = require("mongodb");
 
 const golfersRouter = express.Router();
+const url = process.env.MONGODB_URI || "";
+const dbName = "golf-tracker";
 
 golfersRouter.route("/").get((req, res) => {
+	(async function mongo() {
+		let client;
+		try {
+			console.log("url", url);
+			console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
+			client = await MongoClient.connect(
+				"mongodb+srv://bspangler21:CM2xP2C2Ul5jLf7l@spangdev.xsqup9m.mongodb.net/?retryWrites=true&w=majority"
+			);
+			debug("Connected to the MongoDb server");
 
-  res.send("golfers");
+			// Create instance of mongo database
+
+			const db = client.db(dbName);
+
+			const golfers = await db.collection("golfers").find().toArray();
+
+			res.json(golfers);
+			res.render("golfers", {
+				golfers,
+			});
+		} catch (error) {
+			debug(error.stack);
+		}
+		// client.close();
+	})();
+	// res.send("golfers", golfers);
 	// res.send("Golfers", {
-  //   golfers,
-  // });
+	//   golfers,
+	// });
 });
 
 golfersRouter.route("/:id").get((req, res) => {
 	const id = req.params.id;
-	res.send("golfer", {
-		golfer: golfers[id],
-	});
+	(async function mongo() {
+		let client;
+		try {
+			client = await MongoClient.connect(
+				"mongodb+srv://bspangler21:CM2xP2C2Ul5jLf7l@spangdev.xsqup9m.mongodb.net/?retryWrites=true&w=majority"
+			);
+			debug("Connected to the MongoDb server");
+
+			// Create instance of mongo database
+			const db = client.db(dbName);
+
+			const golfer = await db
+				.collection("golfers")
+				.findOne({ _id: new ObjectID(id) });
+
+			res.json(golfer);
+			res.render("golfer", {
+				golfer,
+			});
+		} catch (error) {
+			debug(error.stack);
+		}
+		// client.close();
+	})();
 });
 
 module.exports = golfersRouter;
